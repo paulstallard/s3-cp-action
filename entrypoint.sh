21d9471cc48dadefc291d262f9c11751236e6190
paulstallard <50678256+paulstallard@users.noreply.github.com>
2020-04-20 12:14:00 +0100

    Update entrypoint.sh
    Handle file lists

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
-            --region ${AWS_REGION} $*
-
+for f in ${FILE_LIST}
+do
+  aws s3 cp ${f} s3://${AWS_S3_BUCKET} --region ${AWS_REGION} $*
+done
 rm -rf ~/.aws
