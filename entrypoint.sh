f8d04d59358d50963b7a5098b2ee9bff657ed5de
Paul Stallard <50678256+paulstallard@users.noreply.github.com>
2021-01-11 18:02:09 +0000

    Added step to create zip from individual csv files

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
+zip csvfiles.zip *.csv
 for f in ${FILE_LIST}
 do
   aws s3 cp ${f} s3://${AWS_S3_BUCKET} --region ${AWS_REGION} $*
